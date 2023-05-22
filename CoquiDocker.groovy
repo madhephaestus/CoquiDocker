377569061998becc0688f3eca6c05274d577aff9
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-22 11:48:56 -0400

    add code

--- CoquiDocker.groovy
+++ CoquiDocker.groovy
@@ -1,18 +1,21 @@
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
 
+OkHttpClient client = new OkHttpClient();
 
-        OkHttpClient client = new OkHttpClient();
+String url = "http://[::1]:5002//api/tts?text=test%20the%20text%20to%20speech&speaker_id=p376&style_wav=&language_id=HTTP/1.1"; // Replace with your URL
 
-        String url = "http://[::1]:5002//api/tts?text=test%20the%20text%20to%20speech&speaker_id=p376&style_wav=&language_id="; // Replace with your URL
+Request request = new Request.Builder()
+		.url(url)
+		.get()
+		.build();
 
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            // Handle the response as needed
-            String responseBody = response.body().string();
-            System.out.println("Response Body: " + responseBody);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+try {
+	Response response = client.newCall(request).execute()
+	// Handle the response as needed
+	String responseBody = response.body().string();
+	System.out.println("Response Body: " + responseBody);
+} catch (IOException e) {
+	e.printStackTrace();
+}
