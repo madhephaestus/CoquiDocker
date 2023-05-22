cbbe791b7a5d62b83a2dd597708654addcffbab2
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-22 11:46:33 -0400

    Ignore the project files

--- CoquiDocker.groovy
+++ CoquiDocker.groovy
@@ -1,1 +1,18 @@
-// code here
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://[::1]:5002//api/tts?text=test%20the%20text%20to%20speech&speaker_id=p376&style_wav=&language_id="; // Replace with your URL
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            // Handle the response as needed
+            String responseBody = response.body().string();
+            System.out.println("Response Body: " + responseBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
